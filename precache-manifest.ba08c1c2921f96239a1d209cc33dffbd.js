280a075b8695130fb2352129750050790f88f694
ShaliniR8 <1705820@kiit.ac.in>
2020-05-09 23:53:00 +0530

    Updates

---
+++ precache-manifest.ba08c1c2921f96239a1d209cc33dffbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a91d552506d340871946e9af1b6ed6",
+    "url": "/ShaliniR8/react-i-will.git/index.html"
+  },
+  {
+    "revision": "7dd287ba7ec76ee5a1cd",
+    "url": "/ShaliniR8/react-i-will.git/static/css/main.4dc632e4.chunk.css"
+  },
+  {
+    "revision": "ab911747f7910db5c6b2",
+    "url": "/ShaliniR8/react-i-will.git/static/js/2.cc726ff5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShaliniR8/react-i-will.git/static/js/2.cc726ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd287ba7ec76ee5a1cd",
+    "url": "/ShaliniR8/react-i-will.git/static/js/main.095816e1.chunk.js"
+  },
+  {
+    "revision": "251920d2c514c03d30e2",
+    "url": "/ShaliniR8/react-i-will.git/static/js/runtime-main.11f0888b.js"
+  }
+]);
